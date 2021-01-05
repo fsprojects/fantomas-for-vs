06d69489cd8a90fde31d9cbc69b837996b56b3d2
asti <asti@dynz.net>
2021-01-05 21:22:42 +0530

    Add Editor for development

--- src/FantomasOptionsDialogPage.cs
+++ src/FantomasOptionsDialogPage.cs
@@ -2,10 +2,11 @@
 {
     using Microsoft.VisualStudio.Shell;
     using System.Windows;
+    using System.Windows.Controls;
 
     public class FantomasOptionsDialogPage : UIElementDialogPage
     {
-        private FantomasOptionsPage2 _page;
-        protected override UIElement Child => _page ??= new FantomasOptionsPage2();
+        //private FantomasOptionsPage2 _page;
+        protected override UIElement Child => new Label { Content = "Under construction" };
     }
 }
