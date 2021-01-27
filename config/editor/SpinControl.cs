7655a6126d45828b38afff022c307ac4dbe64f80
asti <asti@dynz.net>
2021-01-27 19:15:38 +0530

    Add SpinControl

---
+++ config/editor/SpinControl.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+
+namespace FantomasVs.Editor
+{
+    [TemplatePart(Name = "PART_UpButton", Type = typeof(RepeatButton))]
+    [TemplatePart(Name = "PART_DownButton", Type = typeof(RepeatButton))]
+    [TemplatePart(Name = "PART_Text", Type = typeof(TextBox))]
+    public class SpinControl : Control
+    {
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        private RepeatButton UpButtonElement;
+        private RepeatButton DownButtonElement;
+        private TextBox TextElement;
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(int), typeof(SpinControl), new PropertyMetadata());
+
+        public override void OnApplyTemplate()
+        {
+            if (UpButtonElement != null) UpButtonElement.Click -= OnUpClick;
+            if (DownButtonElement != null) DownButtonElement.Click -= OnDownClick;
+
+            UpButtonElement = GetTemplateChild("PART_UpButton") as RepeatButton;
+            DownButtonElement = GetTemplateChild("PART_DownButton") as RepeatButton;
+            TextElement = GetTemplateChild("PART_Text") as TextBox;
+
+            if (UpButtonElement != null) UpButtonElement.Click += OnUpClick;
+            if (DownButtonElement != null) DownButtonElement.Click += OnDownClick;
+        }
+
+        private void OnUpClick(object _, object e) => Value += 1;
+        private void OnDownClick(object _, object e) => Value -= 1;
+
+    }
+}
