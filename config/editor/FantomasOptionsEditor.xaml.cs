5f0faa55830de2cff6f7011442b075669649bc2f
asti <asti@dynz.net>
2021-01-27 19:18:17 +0530

    Add property editor ui

--- config/editor/FantomasOptionsEditor.xaml.cs
+++ config/editor/FantomasOptionsEditor.xaml.cs
@@ -1,15 +1,25 @@
 ﻿namespace FantomasVs.Editor
 {
     using System.Windows.Controls;
+    using System.Windows.Data;
 
     /// <summary>
     /// Interaction logic for FantomasOptionsPage2.xaml
     /// </summary>
     public partial class FantomasOptionsEditor : UserControl
     {
+        public FantomasOptionsPage FantomasOptions { get; }
+        
+        public EditorPage<FantomasOptionsPage> Editor { get; }
+
         public FantomasOptionsEditor()
         {
             InitializeComponent();
+            FantomasOptions = new FantomasOptionsPage();
+            Editor = new EditorPage<FantomasOptionsPage>(FantomasOptions);
+            DataContext = Editor;
+            PropertyView.ItemsSource = Editor.View;
         }
+
     }
 }
