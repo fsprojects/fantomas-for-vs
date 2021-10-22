452c8cb166e7e05052f6806121539b8e7d7bcae5
Asti <rahul.ramakumar@walmart.com>
2021-10-22 11:38:02 +0530

    Automate all the way!

--- update.ps1
+++ update.ps1
@@ -1,17 +1,25 @@
 $env:GIT_REDIRECT_STDERR = '2>&1'
 
+function getTag {
+    $latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
+    $latestTag = $latestTag.Trim('v', '.')
+    [Version]::Parse($latestTag)
+}
+
 pushd ./fantomas-stable
 
 git checkout master
+
+git pull
 
 # Get new tags from remote
 git fetch --tags
 
 # Get latest tag name
-$latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
+$latestTag = getTag
 
 # Checkout latest tag
-git checkout $latestTag
+git checkout tags/v$latestTag
 
 popd
 
@@ -22,3 +30,14 @@ git checkout master
 git pull
 
 popd
+
+pushd ./src
+
+$template = cat -Raw template.vsixmanifest
+$ver = getTag
+$nextVersion = [Version]::new($ver.Major, $ver.Minor, $ver.Build + 1)
+$manifest = $template.Replace("{{latestTag}}", $latestTag).Replace("{{version}}", $nextVersion)
+$manifest | Out-File -FilePath .\source.extension.vsixmanifest -Encoding utf8
+
+popd
+
